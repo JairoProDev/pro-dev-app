4d9da2c41aed0b2f1133f3587712282d22a84eaa
Jairo Saul Salas Quiñones Pro-Dev <JairoProDev@gmail.com>
2023-08-09 20:25:30 -0500

    nice

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<div className="inline-block max-w-lg text-center justify-center">
 				<h1 className={title()}>Jairo Saul&nbsp;</h1>
+				<br />				
 				<h1 className={title({ color: "violet" })}>Pro-Dev | Sci-Tech&nbsp;</h1>
 				<br />
 				<h1 className={title()}>
